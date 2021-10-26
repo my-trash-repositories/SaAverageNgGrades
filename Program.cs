e8e19d923e263ef9a457499b60878e950d2f54f1
Jerome <josejeromelalunio@gmail.com>
2021-10-26 09:10:49 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            getAverage(getInput());
+        }
+
+        private static double[] getInput()
+        {
+            String[] subjects = { "English", "Math", "Science", "Filipino", "History" };
+            List<double> grades = new List<double>();
+            Console.WriteLine("Enter Grades");
+
+            int index = 0;
+            while (grades.Count < subjects.Length)
+            {
+                double input;
+                Console.WriteLine(subjects[index] + " : ");
+                if (double.TryParse(Console.ReadLine(), out input))
+                {
+                    grades.Add(input);
+                    index++;
+                }
+                else Console.WriteLine("Please enter a valid number");
+            }
+            return grades.ToArray();
+        }
+
+        private static void getAverage(double[] grades)
+        {
+            double average = 0;
+            foreach (double grade in grades) average += grade;
+            average /= grades.Length;
+            Console.WriteLine($"the average is {average}");
+        }
+    }
+}
